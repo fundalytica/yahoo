6840c1cd38e623097b23d611100c6b31a2961f40
fotios <fotios@fundalytica.com>
2020-11-20 17:03:42 +0000

    use utils to get us market open

--- yahoo-quote.py
+++ yahoo-quote.py
@@ -3,7 +3,7 @@ import datetime, pytz
 
 import argparse
 
-from utils import utils
+from utils import utils, stock
 from yahoo import Yahoo
 
 def run():
@@ -16,12 +16,12 @@ def run():
     yahoo = Yahoo()
     info = yahoo.request_quote(symbol)
 
-    bid = info['bid']
     ask = info['ask']
     previousClose = info['previousClose']
 
-    isUSMarketOpen = (bid != 0 and ask != 0)
     nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    isUSMarketOpen = stock.isUSMarketOpen(nyc_datetime)
+
     date_fmt = "%B %d, %Y"
     time_fmt = "%I:%M:%S %p"
 
