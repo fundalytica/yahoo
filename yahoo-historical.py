8848443e9fbdfd3e90509e1b9db59fa66b6e0153
fotios <fotios@fundalytica.com>
2020-11-20 07:35:19 +0000

    add save option and write data frame to disk

--- yahoo-historical.py
+++ yahoo-historical.py
@@ -1,12 +1,15 @@
 import json
 
 import argparse
-
 from yahoo import Yahoo
+
+from utils import utils, data
 
 def run():
     argparser = argparse.ArgumentParser(description='Yahoo Finance Historical Data')
     argparser.add_argument("-s", "--symbol", help="stock symbol", required=True)
+    argparser.add_argument("--save", action='store_true', help="save data")
+
     args = argparser.parse_args()
 
     symbol = args.symbol
@@ -16,5 +19,15 @@ def run():
 
     json = df.to_json(orient='index')
     print(json)
+
+    if args.save:
+        path = utils.file_path(__file__)
+        name = utils.file_name(__file__)
+
+        csv = True
+        extension = f'.{"csv" if csv else "pkl"}'
+
+        file = f'{path}/{name}/{symbol}{extension}'
+        data.df_write(df, file, verbose=True)
 
 run()
